21726778a697c46da0e16ac1e84ce2c238444fb0
Devin Sweeting and Phil Mass <phil@>
2018-10-22 13:49:52 -0700

    add foreach to create deck of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+  $("#make-deck").click(function() {
+    var values = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
+    var suits = ["hearts", "diamonds", "spades", "clubs"];
+    var cards = [];
 
+  suits.forEach(function(suit) {
+    values.forEach(function(value) {
+        cards.push(value + " of " + suit);
+        console.log(value + " of " + suit);
+      });
+    });
+  })
 });
