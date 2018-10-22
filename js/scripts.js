13e23aa6853049882be56c68e6f55db1a59f7f44
Devin Sweeting and Phil Mass <phil@>
2018-10-22 16:48:01 -0700

    add identifying notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
+  // Deck of Cards
   $("#make-deck").click(function() {
     var values = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
     var suits = ["hearts", "diamonds", "spades", "clubs"];
     var cards = [];
 
-  $("#cardList").empty();
-    suits.forEach(function(suit) {
-      values.forEach(function(value) {
-          cards.push(value + " of " + suit);
-          $("#cardList").append("<li>" + value + " of " + suit + "</li>");
-          console.log(value + " of " + suit);
+    $("#cardList").empty();
+      suits.forEach(function(suit) {
+        values.forEach(function(value) {
+            cards.push(value + " of " + suit);
+            $("#cardList").append("<li>" + value + " of " + suit + "</li>");
+            console.log(value + " of " + suit);
       });
     });
   })
@@ -31,6 +32,7 @@ $(document).ready(function() {
     $("#groceryForm").hide();
   });
 
+  // WORD COUNT
   $("#words-form").submit(function(event){
     event.preventDefault();
     var wordList = $("#words").val();
