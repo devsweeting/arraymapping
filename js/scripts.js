cce78d0bb6ea7026cdacfc8fb97ee3f1fba9fd84
Devin Sweeting and Phil Mass <phil@>
2018-10-22 14:42:07 -0700

    create grocery form, display grocery list

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,30 @@ $(document).ready(function() {
     var suits = ["hearts", "diamonds", "spades", "clubs"];
     var cards = [];
 
-  suits.forEach(function(suit) {
-    values.forEach(function(value) {
-        cards.push(value + " of " + suit);
-        console.log(value + " of " + suit);
+  $("#cardList").empty();
+    suits.forEach(function(suit) {
+      values.forEach(function(value) {
+          cards.push(value + " of " + suit);
+          $("#cardList").append("<li>" + value + " of " + suit + "</li>");
+          console.log(value + " of " + suit);
       });
     });
   })
+  // GROCERY FORM
+  $("#groceryForm").submit(function(event) {
+    var groceryList = $("#list").val()
+    var groceryArray = groceryList.split(", ");
+    groceryArray.sort();
+    console.log(groceryArray)
+    var groceryCap = groceryArray.map(function(word) {
+      return word.toUpperCase();
+    });
+    event.preventDefault();
+    console.log(groceryCap)
+    $("#groceryList").empty();
+    groceryCap.forEach(function(word){
+      $("#groceryList").append("<li>" + word + "</li>");
+    });
+    $("#groceryForm").hide();
+  });
 });
